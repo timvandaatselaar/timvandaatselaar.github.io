7edd156b00255c61b99fcdea7bb6bc260b3836bf
timvandaatselaar <timvandaatselaar@users.noreply.github.com>
2022-02-09 08:55:25 +0000

    deploy: 813ac7b9b4f81ed3e079df6e3938bbe15ddfcc09

--- .nuxt/layouts.d.ts
+++
@@ -1,7 +0,0 @@
-import { ComputedRef, Ref } from 'vue'
-export type LayoutKey = string
-declare module '/Users/tim/projects/timvandaatselaar.github.io/node_modules/nuxt3/dist/pages/runtime/composables' {
-  interface PageMeta {
-    layout?: false | LayoutKey | Ref<LayoutKey> | ComputedRef<LayoutKey>
-  }
-}
--- app.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="bg-white antialiased dark:bg-slate-900 dark:text-white">
-    <NuxtPage />
-  </div>
-</template>
